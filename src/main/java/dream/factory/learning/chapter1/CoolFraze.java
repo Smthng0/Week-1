cec8f93106c149dc08d8177e7d47c7173a3a797a
Smthng0 <Anonymus2304@gmail.com>
2017-10-31 17:09:27 +0100

    kraj radnog dana...

---
+++ src/main/java/dream/factory/learning/chapter1/CoolFraze.java
@@ -0,0 +1,16 @@
+package dream.factory.learning.chapter1;
+
+public class CoolFraze {
+    public static void main (String[] arg) {
+        String [] prvaLista = {"Jako", "Naj", "Masu", "Podosta"};
+        String [] drugaLista = {"Super", "Puno", "Stvarno", "GOSU" };
+        String [] trecaLista = {"Cool", "Guba", "Gala", "Groovy", "IMBA", "-etina"};
+
+        int oneLength = prvaLista.length;
+        int twoLength = drugaLista.length;
+        int threeLength = trecaLista.length;
+
+
+    }
+
+}
