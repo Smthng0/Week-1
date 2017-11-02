c5ff17d40e8cfe05167862ff1279787c14d34c96
Smthng0 <Anonymus2304@gmail.com>
2017-11-02 10:01:37 +0100

    radi bez cheat-a

--- src/main/java/dream/factory/learning/chapter1/FridgeExercise.java
+++ src/main/java/dream/factory/learning/chapter1/FridgeExercise.java
@@ -3,7 +3,6 @@ package dream.factory.learning.chapter1;
 /*
 page 20, exercise 1... there are 8 parts of code that are scrambled up and i need to sort them to make a working java program...
 i can put as many curly braces as i want (because some are missing), the program needs to print out "a-b c-d"
-i have cheated a little and put "break" in the first if (was not part of the scrambled code) to get rid of the last "-"
  */
 
 public class FridgeExercise {
@@ -12,12 +11,6 @@ public class FridgeExercise {
         int x = 3;
 
         while (x > 0) {
-
-            if (x == 1) {
-                System.out.print("d");
-                x = x - 1;
-                break;
-            }
 
             if (x == 2) {
                 System.out.print("b c");
@@ -29,6 +22,12 @@ public class FridgeExercise {
 
             x = x - 1;
             System.out.print("-");
+
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+
 
         }
 
